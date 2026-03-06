df737433a268647e3f146ffd5aa236d666900024
becker63 <johnsontaylor6320@gmail.com>
2026-03-06 17:29:46 -0500

    flake

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Bun + Vite + Playwright + Pre-commit multi-platform dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    git-hooks.url = "github:cachix/git-hooks.nix";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      git-hooks,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        preCommit = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            # Secret scanning
+            trufflehog.enable = true;
+            ripsecrets.enable = true;
+
+            # Hygiene
+            nixfmt.enable = true;
+            end-of-file-fixer.enable = true;
+            trim-trailing-whitespace.enable = true;
+          };
+        };
+      in
+      {
+        checks.pre-commit-check = preCommit;
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bun
+          ]
+          ++ preCommit.enabledPackages;
+
+          shellHook = ''
+            ${preCommit.shellHook}
+          '';
+        };
+      }
+    );
+}
